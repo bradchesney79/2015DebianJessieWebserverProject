4bdd0b1b1869a88b8da15958acf38d91329510a9
Brad Chesney <bradchesney79@gmail.com>
2015-11-17 19:27:04 -0500

    bash configure exim4

--- build-script.sh
+++ build-script.sh
@@ -669,7 +669,24 @@ printf "\n########## SET MAIL CONFIGS ###\n"
 
 sed -i "s/dc_eximconfig_configtype=.*/dc_eximconfig_configtype='internet'/" /etc/exim4/update-exim4.conf.conf
 sed -i "s/dc_other_hostnames=.*/dc_other_hostnames=''/" /etc/exim4/update-exim4.conf.conf
+
+#
+sed -i "s/dc_local_interfaces=.*/dc_local_interfaces=''/" /etc/exim4/update-exim4.conf.conf
+
+#
+sed -i "s/dc_readhost=.*/dc_readhost=''/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_relay_domains=.*/dc_relay_domains=''/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_minimaldns=.*/dc_minimaldns='false'/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_relay_nets=.*/dc_relay_nets=''/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_smarthost=.*/dc_smarthost=''/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/CFILEMODE=.*/CFILEMODE='644'/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_use_split_config=.*/dc_use_split_config='false'/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_hide_mailname=.*/dc_hide_mailname=''/" /etc/exim4/update-exim4.conf.conf
+sed -i "s/dc_mailname_in_oh=.*/dc_mailname_in_oh='true'/" /etc/exim4/update-exim4.conf.conf
+
 sed -i "s/dc_localdelivery=.*/dc_localdelivery='maildir_home'/" /etc/exim4/update-exim4.conf.conf
+
+echo "$DOMAIN" > /etc/mailname
 
 printf "\n########## REDIRECT SERVER MAIL TO A REAL WORLD ADDRESS ###\n"
 
