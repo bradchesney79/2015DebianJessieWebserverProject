fb3db55745ccde3ad4a3011292434402dba78565
Brad Chesney <bradchesney79@gmail.com>
2015-12-06 03:41:19 -0500

    added usage

--- add-web-person-user.sh
+++ add-web-person-user.sh
@@ -1,21 +1,63 @@
 #!/bin/bash
 
-#SET CONSTANTS 3/3 required
-#1 username
+######################################################################
+######################################################################
 
-#2 associated website group
+#####NOTES & SNIPPETS#####
 
-#3 sudo access, "true"/"false"
+######################################################################
+######################################################################
 
-adduser $1 --add_extra_groups $2
+#based upon:
+# 
+# http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 
-if [ $3 ]
+
+usage(){
+	echo "Usage: $0"
+	echo ""
+	echo "One mandatory argument"
+	echo "One optional argument, TRUE||FALSE, space delimited"
+	echo ""
+	echo "USERNAME (bradchesney79)*"
+	echo ""
+	echo "SUDO USER (FALSE)✓"
+	echo ""
+	echo "* denotes a required argument"
+	echo "✓ denotes a default value"
+	exit 1
+}
+ 
+# invoke  usage
+# call usage() function if parameters not supplied
+[[ $# -eq 0 ]] && usage
+
+if [ -z "$1" ]
+then usage
+fi
+
+if [ "$1" == "-h" ]; then
+then usage
+fi
+
+if [ "$1" == "--help" ]; then
+then usage
+fi
+
+adduser "$1"
+
+
+
+if [ "$2" ]
   then
-    echo "Adding User to sudoers"
-    echo "$1 ALL=(ALL:ALL) ALL" | (EDITOR="tee -a" visudo)
+	if [ "$2" == TRUE ]
+	  then
+	  echo "Adding User to sudoers"
+	  echo "$1 ALL=(ALL:ALL) ALL" | (EDITOR="tee -a" visudo)
+  fi
 fi
 
 cat /etc/group | tr '\n' ' '
 
 echo "How to give a user access to other groups:"
-echo "usermod -a -groups <existing-group> [<existing-group>]..."
+echo "usermod -a -groups <pre-existing-group> [<pre-existing-group>]..."
