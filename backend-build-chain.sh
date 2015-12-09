3c2f1d995f4286325e71f75948be8d2a67286614
Brad Chesney <bradchesney79@gmail.com>
2015-12-09 07:49:14 -0500

    added a restful PHP library composer require

--- backend-build-chain.sh
+++ backend-build-chain.sh
@@ -2,5 +2,16 @@
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
+{
+    "require": {
+        "nategood/httpful": "*"
+}
+
 apt-get install nodejs npm
+
+npm install grunt lodash jquery angularjs spritesmith
+
+
+
+
 
