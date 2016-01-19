c2a9fb80b1c398ee5dbd96d4a3776767e6ea17ba
Brad Chesney <bradchesney79@gmail.com>
2016-01-19 16:10:48 -0500

    init.sh clean up after itself

--- init.sh
+++ init.sh
@@ -8,9 +8,11 @@ pushd /root/bin
 
 wget https://github.com/bradchesney79/2015DebianJessieWebserverProject/archive/master.zip
 
-unzip master.zip
+unzip /root/bin/master.zip
 
 cp -R /root/bin/2015DebianJessieWebserverProject-master/* /root/bin/
 
 chmod -R 770 /root/bin/*
 find /root/bin/* -type d -exec chmod -R 771 {} \;
+
+rm -rf /root/init.sh /root/bin/2015DebianJessieWebserverProject /root/bin/master.zip
