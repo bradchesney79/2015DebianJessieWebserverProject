3756692689b9caec7c25133356958c9a37245448
Brad Chesney <bradchesney79@gmail.com>
2016-01-21 00:19:33 -0500

    the perils of MySQL 5.7 & Sentinel have been put to rest

--- scripts/sentinel.sh
+++ scripts/sentinel.sh
@@ -1,10 +1,10 @@
 
 printf "\n########## CONFIGURE SENTINEL DB TABLES###\n"
 
-# "datalord:seconddummypassword"
-
 mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<<"CREATE DATABASE $SENTINELDB"
 
-mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$DBWEBUSER'@'localhost' IDENTIFIED BY '$DBWEBUSERPASSWORD' WITH GRANT OPTION;"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$DEFAULTSITEDBUSER'@'localhost' IDENTIFIED BY '$DEFAULTSITEDBUSERPASSWORD' WITH GRANT OPTION;"
 
-mysql -u"$DBWEBUSER" -p"$DBWEBUSERPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
+sed -i -e 's/\x270000-00-00 00:00:00\x27/CURRENT_TIMESTAMP/g' $WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql
+
+mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBUSERPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
