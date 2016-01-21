a7ef3c4b95408bcd2f67596137a60567ac6e0767
Brad Chesney <bradchesney79@gmail.com>
2016-01-21 06:21:27 -0500

    tweaking sentinel db install for mysql 5.7

--- scripts/sentinel.sh
+++ scripts/sentinel.sh
@@ -7,4 +7,6 @@ mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINEL
 
 sed -i -e 's/\x270000-00-00 00:00:00\x27/CURRENT_TIMESTAMP/g' $WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql
 
+echo mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBUSERPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
+
 mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBUSERPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
