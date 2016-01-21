c723861c8930f958e7feabef51ed8edffb370db2
Brad Chesney <bradchesney79@gmail.com>
2016-01-21 18:01:58 -0500

    bad password & forgot to flush privileges

--- scripts/sentinel.sh
+++ scripts/sentinel.sh
@@ -1,14 +1,16 @@
 
 printf "\n########## CONFIGURE SENTINEL DB TABLES###\n"
 
-mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<<"CREATE DATABASE $SENTINELDB"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "CREATE DATABASE $SENTINELDB"
 
 mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$DEFAULTSITEDBUSER'@'localhost' IDENTIFIED BY '$DEFAULTSITEDBUSERPASSWORD' WITH GRANT OPTION;"
+
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "FLUSH PRIVILEGES;"
 
 echo "sed -i -e 's/\x270000-00-00 00:00:00\x27/CURRENT_TIMESTAMP/g' $WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
 
 sed -i -e 's/\x270000-00-00 00:00:00\x27/CURRENT_TIMESTAMP/g' $WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql
 
-echo "mysql -u\"$DEFAULTSITEDBUSER\" -p\"$DEFAULTSITEDBUSERPASSWORD\" -D\"$SENTINELDB\" < \"$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql\""
+echo "mysql -u\"$DEFAULTSITEDBUSER\" -p\"$DEFAULTSITEDBPASSWORD\" -D\"$SENTINELDB\" < \"$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql\""
 
-mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBUSERPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
+mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBPASSWORD" -D"$SENTINELDB" < "$WEBROOT/vendor/cartalyst/sentinel/schema/mysql.sql"
