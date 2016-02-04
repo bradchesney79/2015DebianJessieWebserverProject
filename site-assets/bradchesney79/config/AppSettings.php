67f6093dcf3bdedf8b1d8720da1a871b4a02aa2a
Brad Chesney <bradchesney79@gmail.com>
2016-02-04 16:23:14 -0500

    Namespacing

---
+++ site-assets/bradchesney79/config/AppSettings.php
@@ -0,0 +1,13 @@
+namespace bradchesney79/config/AppSettings
+
+
+// Application Settings //
+
+class AppSettings {
+
+    var $WEBROOT       = "/var/www";
+    var $DOCUMENTROOT  = $WEBROOT . "/https";
+    var $HOST          = "www";
+    var $DOMAIN        = "rustbeltrebellion.com";
+
+}
